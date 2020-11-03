fa47f426c30bf9b73aa05cbfe0bc3f8f3f727168
dev01997D <devnandan01997@gmail.com>
2020-11-03 23:19:48 +0530

    Added: Multiple employees are added to database payroll_service at same simultaneously

---
+++ main/java/com/blz/employeepayrollsql/model/CustomThreadException.java
@@ -0,0 +1,9 @@
+package com.blz.employeepayrollsql.model;
+
+@SuppressWarnings("serial")
+public class CustomThreadException extends Exception{
+
+	public CustomThreadException(String msg) {
+		super(msg);
+	}
+}
